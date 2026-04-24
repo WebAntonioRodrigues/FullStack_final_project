ed80d4bca5a86ff2c10fa88b12538f34e34a1cd7
WebAntonioRodrigues <webantoniosilvards@gmail.com>
2026-04-25 00:00:38 +0100

    json file added to data

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,7 +1,7 @@
 function Catalog() {
 	return (
 		<div>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quidem corrupti magnam, provident maxime in at quasi animi id molestiae.</p>
+			
 		</div>
 	);
 }
