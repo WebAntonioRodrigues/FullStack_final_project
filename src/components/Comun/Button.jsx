7921567565abeb69df8b5734a21a2e87e6889113
WebAntonioRodrigues <webantoniosilvards@gmail.com>
2026-05-09 03:49:42 +0100

    errors fixed

--- src/components/Comun/Button.jsx
+++ src/components/Comun/Button.jsx
@@ -1,6 +1,6 @@
 function Button(props) {
 	return (
-		<button className={props.className}>
+		<button className={props.className} onClick={props.onClick}>
 			<span className={props.textClassName}>{props.text}</span>
 		</button>
 	);
